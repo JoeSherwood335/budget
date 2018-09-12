cb473eef63b842e48f213e3e8d9c1f6697de982b
Joe Sherwood <joesherwood@vgsjob.org>
2018-09-12 09:56:34 -0400

    yep starting over

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using budget.Models;
 
 namespace budget.Controllers
 {
@@ -29,7 +31,7 @@ namespace budget.Controllers
 
         public IActionResult Error()
         {
-            return View();
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
